22d9bf407c1101833b95e6ab962735003db7b47b
Ubivis <gunnar.steincke@hamburg.de>
2025-03-25 09:42:14 +0100

    Create QuestType.java

---
+++ src/main/java/com/ubivismedia/dungeonlobby/quests/QuestType.java
@@ -0,0 +1,21 @@
+package com.ubivismedia.aidungeon.quests;
+
+/**
+ * Enum representing the different types of quests
+ */
+public enum QuestType {
+    /**
+     * Kill a specific entity or number of entities
+     */
+    KILL,
+    
+    /**
+     * Collect a specific item or number of items
+     */
+    COLLECT,
+    
+    /**
+     * Explore or discover specific locations
+     */
+    EXPLORE
+}
